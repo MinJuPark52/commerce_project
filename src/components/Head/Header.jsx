789cb4bd54124e4f0f1f873496f633e177d04301
MinJuPark52 <als4013@naver.com>
2024-11-07 18:26:10 +0900

    페이지 및 레이아웃 구현

--- src/components/Head/Header.jsx
+++ src/components/Head/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FiUser, FiShoppingCart, FiHeart, FiSearch } from "react-icons/fi";
-import "./HeadStyle.css"; // Header.css를 import
+import "./HeadStyle.css";
 
 const categories = {
   new: ["가죽자켓", "코트", "경량패팅", "단독", "인플루언서 PICK"],
