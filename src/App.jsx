eba8759aa7690613554c42be5777c6575adb3253
MinJuPark52 <als4013@naver.com>
2024-11-07 18:40:14 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import Header from "./components/Header";
-import JoinPage from "./components/joinPage";
+import Header from "./components/Head/Header";
+import JoinPage from "./components/Head/JoinPage"; // 경로 수정 필요
+import LoginPage from "./components/Head/LoginPage";
+import Footer from "./components/Footer/Foo";
+import Home from "./components/main/Home";
 
 const App = () => {
   const location = useLocation(); // 현재 경로를 가져옵니다.
@@ -14,10 +16,13 @@ const App = () => {
       )}
       {/* 로그인 페이지가 아닐 때만 Header 렌더링 */}
       <Routes>
-        <Route path="/" element={<h1>홈</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/join" element={<JoinPage />} />
       </Routes>
+      {location.pathname !== "/login" && location.pathname !== "/join" && (
+        <Footer />
+      )}
     </div>
   );
 };
