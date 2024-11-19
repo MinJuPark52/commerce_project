a67a51c2804564e3f07858929c7f3cb326419844
MinJuPark52 <als4013@naver.com>
2024-11-20 02:50:47 +0900

    배포 전 수정

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElemzentById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/commerce_project/", // GitHub Pages에서 사용할 경로
 });
