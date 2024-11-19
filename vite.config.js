c1996bd16bc0f5e971e83dc78c1e96581e70cf7c
MinJuPark52 <als4013@naver.com>
2024-11-20 02:01:49 +0900

    vite.config 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/commerce-project/", // GitHub Pages에서 사용할 경로 (저장소 이름)
+});
