09eca82be1fe24e191cd57042fbad39d5ac9c6e2
MinJuPark52 <162428070+MinJuPark52@users.noreply.github.com>
2024-11-26 20:09:34 +0900

    Update KakaoLogin.jsx

--- src/components/Head/KakaoLogin.jsx
+++ src/components/Head/KakaoLogin.jsx
@@ -1,10 +1,9 @@
 // src/KakaoLogin.js
 
 const KakaoLogin = () => {
-  const Rest_api_key = "5aa455913969de8a7301ca72f5fc3ad4"; //REST API KEY
   const redirect_uri = "http://localhost:5173/auth"; //Redirect URI
   // oauth 요청 URL
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=&redirect_uri=${redirect_uri}&response_type=code`;
   const handleLogin = () => {
     window.location.href = kakaoURL;
   };
